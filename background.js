5e09993866cc3cdc758dd86d06b4a7a058e24d45
Shohei Fujii <fujii.shohei@gmail.com>
2024-06-02 16:02:57 +0900

    manifest v2 -> v3

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
-chrome.contextMenus.create({
-    "title": "'%s'の語源を検索",
-    "type": "normal",
-    "contexts": ["selection"],
-    "onclick": function(val) {
-        chrome.tabs.create({
-            url: 'http://hidic.u-aizu.ac.jp/result.php?tableName=tango&word='+encodeURIComponent(val.selectionText)
-        });
-    }
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.contextMenus.create({
+		"id": "selected_text_etymology",
+		"title": "'%s'の語源を検索",
+		"type": "normal",
+		"contexts": ["selection"],
+	});
+});
+
+
+chrome.contextMenus.onClicked.addListener(function(val){
+	if (val.menuItemId == "selected_text_etymology"
+		&& val.selectionText) {
+		chrome.tabs.create({
+			url: 'http://hidic.u-aizu.ac.jp/result.php?tableName=tango&word='+encodeURIComponent(val.selectionText)
+		});
+	}
 });
